3e9c5ef492ee0dd12122dff01758a36ec9e858ec
Birat Budhathoki <biratbudhathoki79@gmail.com>
2025-01-10 08:08:11 +0545

    Login section modified

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
 import Booking from "./pages/Booking";
 import Footer from "./components/Footer";
-import Signup from "./pages/Signup";
+
 import PhotoGallery from "./pages/photogallery";
 import BookNow from "./pages/Booknow";
 import HourSelection from "./pages/hourselection";
 import BookingForm from "./pages/Details";
 import ScrollToTop from "./components/ScrollToTop";
+import Login from "./pages/Login";
 
 
 
@@ -30,7 +31,7 @@ const App = () => {
           <Route path="/booknow" element={<Booking />} />
           <Route path="/faq" element={<FAQPage />} />
           <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/login" element={<Signup/>} />
+          <Route path="/login" element={<Login/>} />
           <Route path="/bookstudio" element={<BookNow/>} />
           <Route path="/hourselection" element={<HourSelection/>} />
           
